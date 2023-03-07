5d1e16a36a089c034d2cadd1a0d02f1c8e554366
chami98 <chamikara98@gmail.com>
2023-03-07 12:51:35 +0530

    Changed Ui a bit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [ethAddress, setEthAddress] = useState("");
+  const [responseData, setResponseData] = useState([]);
+
+
+
+  const submitHandler = async () => {
+    const response = await fetch(`https://api.etherscan.io/api?module=account&action=txlistinternal&address=${ethAddress}&startblock=0&endblock=2702578&page=1&offset=10&sort=asc&apikey=77YYM4VXFPMMKIAMMWZWCPC9EXHAVRNA6X`);
+    const data = await response.json();
+    setResponseData(data);
+    await console.log(responseData);
+  };
+
+
+  const handleInput = async (e) => {
+    setEthAddress(e.target.value);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 style={{ marginTop: '20px', fontSize: '32px', fontWeight: 'bold', color: '#007bff' }}>Ethereum Scanner</h1>
+      <input placeholder='Etherum' onChange={(e) => handleInput(e)} style={{
+        padding: '10px',
+        border: '1px solid #ccc',
+        borderRadius: '5px',
+        fontSize: '16px',
+        marginRight: '10px'
+      }} />
+      <button onClick={submitHandler} style={{
+        padding: '10px',
+        backgroundColor: '#007bff',
+        color: '#fff',
+        border: 'none',
+        borderRadius: '5px',
+        fontSize: '16px',
+        cursor: 'pointer'
+      }}>Submit</button>
+
+      {
+        responseData.status == 1 ? (<div className='table-responsive'><table class="table table-striped" style={{ maxWidth: '80%', marginLeft: 'auto', marginRight: 'auto' }}>
+          <thead>
+            <tr>
+              <th scope="col">BlockNumber</th>
+              <th scope="col">From</th>
+              <th scope="col">Gas</th>
+              <th scope="col">Hash</th>
+              <th scope="col">Time Stamp</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            {responseData.result.map((tx, index) => (
+              <tr key={index}>
+                <td>{tx.blockNumber}</td>
+                <td>{tx.from}</td>
+                <td>{tx.gas}</td>
+                <td>{tx.hash}</td>
+                <td>{tx.timeStamp}</td>
+              </tr>
+            ))}
+
+          </tbody>
+        </table></div>) : <>
+          <h2 style={{ marginTop: '30vh', color: 'tomoto' }}>Transaction not found!</h2>
+        </>
+      }
+    </div >
   );
 }
 
